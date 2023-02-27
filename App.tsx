e6cf7979f1b39159649b8cf97e69a93942970673
YasminBatista <yasminbdsouza83@gmail.com>
2023-02-27 10:22:51 -0300

    foi

--- App.tsx
+++ App.tsx
@@ -1,79 +1,20 @@
-import React, { useState } from 'react'
-import { View, StyleSheet,Text ,Image } from 'react-native'
-import logo from "./assets/logo.png"
+import React, { useState,Dispatch, SetStateAction } from 'react';
+import Page1 from "./Pages/Page1";
+import Page2 from './Pages/Page2';
 
-import Toggle from './Toggle'
-
-export default function App() {
-  const [flexDirection, setFlexDirection] = useState('row')
-  const [justifyContent, setJustifyContent] = useState('center')
-  const [alignItems, setAlignItems] = useState('center')
-  const layoutStyle = { flexDirection, justifyContent, alignItems }
-
-  const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-  const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
-  return (
-    <View style={styles.container}>
-      <Toggle
-        label={'Primary axis (flexDirection)'}
-        value={flexDirection}
-        options={flexDirectionOptions}
-        onChange={(option) => {
-          setFlexDirection(option)
-        }}
-      />
-      <Toggle
-        label={`${primaryAxis} distribution (justifyContent)`}
-        value={justifyContent}
-        options={justifyContentOptions}
-        onChange={(option) => {
-          setJustifyContent(option)
-        }}
-      />
-      <Toggle
-        label={`${secondaryAxis} alignment (alignItems)`}
-        value={alignItems}
-        options={alignItemsOptions}
-        onChange={(option) => {
-          setAlignItems(option)
-        }}
-      />
-      <Image source={logo} style={styles,img}></Image> 
-      <View style={[styles.layout, layoutStyle]}>
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-      </View>
-    </View>
-  )
+export interface Ipage{
+  setPageI: Dispatch<SetStateAction<number>>
 }
 
-const flexDirectionOptions = ['row', 'column']
-const justifyContentOptions = [
-  'flex-start',
-  'center',
-  'flex-end',
-  'space-around',
-  'space-between',
-]
-const alignItemsOptions = ['flex-start', 'center', 'flex-end', 'stretch']
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  layout: {
-    flex: 1,
-    backgroundColor: '#b39797c',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: '#c48383',
-    margin: 5,
-  },
-  img: {
-    width:50
-    height:50
-  }
-})
+export default function App() {
+  const [page,setPage] = useState(1) 
+  return(
+    <>
+    {
+      page == 1?
+      (<Page1 setPageI=(setPage) />) :
+      (<Page2 setPageI=(setPage) />)
+    }
+    </>
+  )    
+}
